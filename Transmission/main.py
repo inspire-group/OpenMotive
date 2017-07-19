4017ad00f11f4a4e6b568da042185d82362414d4
Jad Bechara <jad@nat-oitwireless-inside-vapornet100-c-1986.Princeton.EDU>
2017-07-19 13:22:45 -0400

    Added 4 running modes to main and run

--- Transmission/main.py
+++ Transmission/main.py
@@ -1,12 +1,15 @@
 from guizero import App, ButtonGroup, PushButton, Text
+import ModeSports, ModeEfficiency
 
 def start():
-    if mode_button.get() == 'A':
-        print("\Sports Mode")
-        import ModeSports
-    else:
-        print("\Efficiency Mode")
-        import ModeEfficiency
+    if mode_button.get() == 'A' and type_button.get() == 'A':
+        print('\nSports Mode - Training')
+    elif mode_button.get() == 'A' and type_button.get() == 'B':
+        print('\nSports Mode - Testing')
+    elif mode_button.get() == 'B' and type_button.get() == 'A':
+        print('\nEfficiency Mode - Training')
+    elif mode_button.get() == 'B' and type_button.get() == 'B':
+        print('\nEfficiency Mode - Testing')
 
 app = App(title = "Transmission")
 
@@ -15,6 +18,10 @@ app_message = Text(app, text = "Vehicle Transmission Sports and\
 mode_message = Text(app, text = "Mode", size = 14,\
 font = "Times New Roman")
 mode_button = ButtonGroup(app, options=[["Sports", "A"], ["Efficiency", "B"]],\
+selected = "A", horizontal = True, align = "left")
+type_message = Text(app, text = "Type", size = 14,\
+font = "Times New Roman")
+type_button = ButtonGroup(app, options=[["Train", "A"], ["Test", "B"]],\
 selected = "A", horizontal = True, align = "left")
 start_button = PushButton(app, command = start, text = "Start")
 
--- Transmission/run.py
+++ Transmission/run.py
@@ -1,19 +1,23 @@
-import sys
+import ModeSports, ModeEfficiency, sys
 
 def usage():
     print('\n========================')
 	print('Transmission USAGE')
 	print('========================\n')
-	print('python3 run.py mode')
+	print('python3 run.py mode type')
 	print('        - mode: spo or eff\n')
+    print('        - type: train or test')
 	exit()
 
-if len(sys.argv) < 2: usage()
+if len(sys.argv) < 3: usage()
 mode = sys.argv[1]
-if mode == 'spo':
-	print('\nMode Sports')
-	import ModeSports
-elif mode == 'eff':
-	print('\nMode Efficiency')
-	import ModeEfficiency
+phase = sys.argv[2]
+if mode == 'spo' and phase == 'train':
+    print('\nSports Mode - Training')
+elif mode == 'spo' and phase == 'test':
+    print('\nSports Mode - Testing')
+elif mode == 'eff' and phase == 'train':
+    print('\nEfficiency Mode - Training')
+elif mode == 'eff' and phase == 'test':
+    print('\nEfficiency Mode - Testing')
 else: usage()
