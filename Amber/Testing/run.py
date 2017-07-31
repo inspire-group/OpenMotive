d0a22e695800dd78e5dc01dca270307a35949b0e
Jad Bechara <jadbechara@hotmail.com>
2017-07-31 15:59:08 -0400

    Finished Testing script

--- Amber/Testing/run.py
+++ Amber/Testing/run.py
@@ -3,46 +3,53 @@ from Cloud import Cloud
 from Hybrid import Hybrid
 import sys
 
+PLATES = [1:'78197', 2:'T638802C', 3:'T640951C', 4:'N49EPU', 5:'382946', 6:'R43HBY',\
+          7:'X35ELG', 8:'V68EPT', 9:'JCG92V', 10:'EEB4657', 11:'5N23', 12:'GDB3917']
+
 def usage():
     print('\n========================')
     print('AMBER USAGE')
     print('========================\n')
-    print('python3 run.py "mode" "fps" "resolution" "video_id" "ip" "plate1, plate2, plate3, ..."')
+    print('python3 run.py "mode" "ip" "perf"')
     print('        - mode: local or cloud or hybrid')
-    print('        - fps: 1, 5, or 10')
-    print('        - resolution: 720 or 1080')
-    print('        - video_id: 1-12')
     print('        - ip: AWS ip (e.g. 0.0.0.0) - required in Cloud and Hybrid modes\n')
+    print('        - perf: performance testing - sets FPS and Resolution to max')
     exit()
 
-if len(sys.argv) < 6 or len(sys.argv) > 7: usage()
-
+PERF = False
+if len(sys.argv) < 2: usage()
 mode = sys.argv[1]
-video_fps = sys.argv[2]
-video_res = sys.argv[3]
-video_id = sys.argv[4]
-if len(sys.argv) == 6:
-    if mode == 'local': usage()
-    lps = sys.argv[5].upper().split(', ')
-else:
-    aws_ip = '-'.join(sys.argv[5].split('.'))
-    lps = sys.argv[6].upper().split(', ')
+if mode not in ['local', 'cloud', 'hybrid']: usage()
+if len(sys.argv) == 2:
+    if mode != 'local': usage()
+elif len(sys.argv) == 3:
+    if sys.argv[2] == 'perf':
+        if mode != 'local': usage()
+        PERF = True
+    else: aws_ip = '-'.join(sys.argv[2].split('.'))
+elif len(sys.argv) == 4:
+    if sys.argv[3] == 'perf': PERF = True
+    else: usage()
+    aws_ip = '-'.join(sys.argv[2].split('.'))
+else: usage()
 
-if mode not in ['local', 'cloud', 'hybrid']\
-or video_fps not in ['1', '5', '10']\
-or video_res not in ['720', '1080']\
-or video_id not in [str(i+1) for i in range(12)]: usage()
+FPS = 10
+RES = 1080
 alpr = None
-if mode == 'local':
-    alpr = Local(fps=int(video_fps), res=int(video_res), vid_id=int(video_id))
-    print('Local Mode')
-elif mode == 'cloud':
-    alpr = Cloud(fps=int(video_fps), res=int(video_res), vid_id=int(video_id), ip=aws_ip)
-    print('Cloud Mode')
-elif mode == 'hybrid':
-    alpr = Hybrid(fps=int(video_fps), res=int(video_res), vid_id=int(video_id), ip=aws_ip)
-    print('Hybrid Mode')
-alpr.add(lps)
-results = alpr.find()
-for lp in results:
-    print('\nFound vehicle ' + lp[0] + ', confidence = ' + lp[1])
+for video_id in range(1, 13):
+    if PERF:
+        if mode == 'local': alpr = Local(fps=FPS, res=RES, vid_id=video_id)
+        elif mode == 'cloud': alpr = Cloud(fps=FPS, res=RES, vid_id=video_id, ip=aws_ip)
+        elif mode == 'hybrid': alpr = Hybrid(fps=FPS, res=RES, vid_id=video_id, ip=aws_ip)
+        print("PERFORMANCE - SPAF: %f" % alpr.find(PLATES[video_id]))
+    else:
+        for video_fps in [1, 5, 10]:
+            if mode == 'local': alpr = Local(fps=video_fps, res=RES, vid_id=video_id)
+            elif mode == 'cloud': alpr = Cloud(fps=video_fps, res=RES, vid_id=video_id, ip=aws_ip)
+            elif mode == 'hybrid': alpr = Hybrid(fps=video_fps, res=RES, vid_id=video_id, ip=aws_ip)
+            print("FPS %d - SPAF: %f" % (video_fps, alpr.find(PLATES[video_id])))
+        for video_res in [720, 1080]:
+            if mode == 'local': alpr = Local(fps=FPS, res=video_res, vid_id=video_id)
+            elif mode == 'cloud': alpr = Cloud(fps=FPS, res=video_res, vid_id=video_id, ip=aws_ip)
+            elif mode == 'hybrid': alpr = Hybrid(fps=FPS, res=video_res, vid_id=video_id, ip=aws_ip)
+            print("RES %d - SPAF: %f" % (video_res, alpr.find(PLATES[video_id])))
